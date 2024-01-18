52a523cdf86d7ea4a8fd1756d22d17eb2685a79e
bitcloud2 <bitcloud22@gmail.com>
2024-01-17 23:04:58 -0800

    finished chap 11

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,10 +1,12 @@
 mod automata;
 mod drunkard;
+mod prefab;
 mod rooms;
 
 use crate::prelude::*;
 use automata::CellularAutomataArchitect;
 use drunkard::DrunkardsWalkArchitect;
+use prefab::apply_prefab;
 use rooms::RoomsArchitect;
 
 trait MapArchitect {
@@ -28,7 +30,8 @@ impl MapBuilder {
             1 => Box::new(RoomsArchitect {}),
             _ => Box::new(CellularAutomataArchitect {}),
         };
-        let mb = architect.new(rng);
+        let mut mb = architect.new(rng);
+        apply_prefab(&mut mb, rng);
         mb
     }
 
